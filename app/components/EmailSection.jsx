fd01713cf446cdcd1e9beced4876291b2f9ff1b6
ashwilliam2001 <arghyapatra340@gmail.com>
2024-05-17 01:02:34 +0530

    hgdvwekgf
    kjeit

--- app/components/EmailSection.jsx
+++ app/components/EmailSection.jsx
@@ -9,36 +9,6 @@ import { PhoneIcon } from "@heroicons/react/24/solid";
 const EmailSection = () => {
     const [emailSubmitted, setEmailSubmitted] = useState(false);
 
-    const handleSubmit = async (e) => {
-        e.preventDefault();
-        const data = {
-            email: e.target.email.value,
-            subject: e.target.subject.value,
-            message: e.target.message.value,
-        };
-        const JSONdata = JSON.stringify(data);
-        const endpoint = "/api/send";
-
-        // Form the request for sending data to the server.
-        const options = {
-            // The method is POST because we are sending data.
-            method: "POST",
-            // Tell the server we're sending JSON.
-            headers: {
-                "Content-Type": "application/json",
-            },
-            // Body of the request is the JSON data we created above.
-            body: JSONdata,
-        };
-
-        const response = await fetch(endpoint, options);
-        const resData = await response.json();
-
-        if (response.status === 200) {
-            console.log("Message sent.");
-            setEmailSubmitted(true);
-        }
-    };
 
     return (
         <section
@@ -63,73 +33,11 @@ const EmailSection = () => {
                     <Link href="linkedin.com">
                         <Image src={LinkedinIcon} alt="Linkedin Icon" />
                     </Link>
-                    <div className="flex items-center gap-2">
-                        <PhoneIcon className="h-9 w-9" />
-                        <span className="text-white">+91-8145895594</span>
-                    </div>
+                <div className="flex items-center gap-2">
+                    < PhoneIcon className="h-9 w-9"/>
+                    <span className="text-white">+91-8145895594</span>
                 </div>
             </div>
-            <div>
-                {emailSubmitted ? (
-                    <p className="text-green-500 text-sm mt-2">
-                        Email sent successfully!
-                    </p>
-                ) : (
-                    <form className="flex flex-col" onSubmit={handleSubmit}>
-                        <div className="mb-6">
-                            <label
-                                htmlFor="email"
-                                className="text-white block mb-2 text-sm font-medium"
-                            >
-                                Your email
-                            </label>
-                            <input
-                                name="email"
-                                type="email"
-                                id="email"
-                                required
-                                className="bg-[#18191E] border border-[#33353F] placeholder-[#9CA2A9] text-gray-100 text-sm rounded-lg block w-full p-2.5"
-                                placeholder="jacob@gmail.com"
-                            />
-                        </div>
-                        <div className="mb-6">
-                            <label
-                                htmlFor="subject"
-                                className="text-white block text-sm mb-2 font-medium"
-                            >
-                                Subject
-                            </label>
-                            <input
-                                name="subject"
-                                type="text"
-                                id="subject"
-                                required
-                                className="bg-[#18191E] border border-[#33353F] placeholder-[#9CA2A9] text-gray-100 text-sm rounded-lg block w-full p-2.5"
-                                placeholder="Just saying hi"
-                            />
-                        </div>
-                        <div className="mb-6">
-                            <label
-                                htmlFor="message"
-                                className="text-white block text-sm mb-2 font-medium"
-                            >
-                                Message
-                            </label>
-                            <textarea
-                                name="message"
-                                id="message"
-                                className="bg-[#18191E] border border-[#33353F] placeholder-[#9CA2A9] text-gray-100 text-sm rounded-lg block w-full p-2.5"
-                                placeholder="Let's talk about..."
-                            />
-                        </div>
-                        <button
-                            type="submit"
-                            className="bg-primary-500 hover:bg-primary-600 text-white font-medium py-2.5 px-5 rounded-lg w-full"
-                        >
-                            Send Message
-                        </button>
-                    </form>
-                )}
             </div>
         </section>
     );
